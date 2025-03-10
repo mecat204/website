cd87bf4f1e89785779ff7aab712a116cbe12e3f3
mecat204 <rixin8297@gmail.com>
2025-03-10 22:41:13 +0800

    Initial commit

---
+++ web_collect/movie/js/data.js
@@ -0,0 +1,654 @@
+var arr = [
+    {
+        "href":"https://tv.cctv.com/live/cctv10/",
+        "cont":"央视网页直播"
+    },
+    {
+        "href":"http://live.yj1211.work/index/home/platforms",
+        "cont":"一个整合 哔哩哔哩 / 虎牙 / 斗鱼 / 网易，直播内容的直播网站"
+    },
+    {
+        "href":"https://zhuiyingmao2.com/",
+        "cont":"追影猫"
+    },
+    {
+        "href":"https://dy.town/",
+        "cont":"电影镇"
+    },
+    {
+        "href":"https://yandex.com/",
+        "cont":"yandex | search"
+    },
+    {
+        "href":"https://www.xsmp3.com/",
+        "cont":"相声随身听"
+    },
+    {
+        "href":"https://www.similarsites.com/",
+        "cont":"similarsites"
+    },
+    {
+        "href":"https://dsxys.pro/",
+        "cont":"大师兄影视"
+    },
+    {
+        "href":"https://ddys.one/",
+        "cont":"低端影视"
+    },
+    {
+        "href":"https://www.libvio.fun/",
+        "cont":"libvio"
+    },
+    {
+        "href":"http://www.kekys.com",
+        "cont":"可可影视"
+    },
+    {
+        "href":"https://www.2bt0.com/",
+        "cont":"重点推荐-不太灵影视"
+    },
+    {
+        "href":"https://www.yinfans.me/",
+        "cont":"音范丝"
+    },
+    {
+        "href":"https://www.netfly.tv/",
+        "cont":"奈飞中文网"
+    },
+    {
+        "href":"https://www.imahuatv.com/",
+        "cont":"麻花影视"
+    },
+    {
+        "href":"https://www.haojuwo.com/",
+        "cont":"星辰影院"
+    },
+    {
+        "href":"https://www.xinjuc.com/",
+        "cont":"新剧坊"
+    },
+    {
+        "href":"https://www.mkvdo.com/",
+        "cont":"MK影视"
+    },
+    {
+        "href":"https://gimy.ai/",
+        "cont":"A-moive|gimy剧迷"
+    },
+    {
+        "href":"https://gfvod.com/",
+        "cont":"干饭影视"
+    },
+    {
+        "href":"https://adys.me/",
+        "cont":"爱迪影视"
+    },
+    {
+        "href":"https://www.kupian.cc/",
+        "cont":"酷片网"
+    },
+    {
+        "href":"https://www.wuji12.com/",
+        "cont":"无极影视"
+    },
+    {
+        "href":"https://www.xigua28.com/",
+        "cont":"西瓜影视"
+    },
+    {
+        "href":"https://www.shipinla.com/",
+        "cont":"视频啦"
+    },
+    {
+        "href":"https://www.4khdr.cn/",
+        "cont":"4K影视"
+    },
+    {
+        "href":"https://tv.cctv.com/live/cctv13/",
+        "cont":"CCTV"
+    },
+    {
+        "href":"https://iui.su/3154/",
+        "cont":"电视直播大全"
+    },
+    {
+        "href":"https://iui.su/531/",
+        "cont":"在线影视收集"
+    },
+    {
+        "href":"https://www.netflixgc.com/",
+        "cont":"netflix"
+    },
+    {
+        "href":"https://85128.net/",
+        "cont":"85电影网"
+    },
+    {
+        "href":"https://iptv-org.github.io/",
+        "cont":"iptv-org"
+    },
+    {
+        "href":"https://letou.us/",
+        "cont":"乐透盒子|IPTV"
+    },
+    {
+        "href":"https://www.potplay.net/potplayer-2024iptv-m3u8",
+        "cont":"potplay-日常分享potplay源地址"
+    },
+    {
+        "href":"https://www.mihoyogift.com/?utm_source=bbs&utm_medium=ys&utm_campaign=store",
+        "cont":"米游铺"
+    },
+    {
+        "href":"https://www.kekefx.com/",
+        "cont":"可可分享"
+    },
+    {
+        "href":"https://act.mihoyo.com/ys/app/interactive-map/index.html?lang=zh-cn#/map/2?shown_types=391&center=2008.50,-1084.00&zoom=-2.50",
+        "cont":"原神地图"
+    },
+    {
+        "href":"https://bbs.mihoyo.com/ys/obc/?bbs_presentation_style=no_header",
+        "cont":"米游社·原神"
+    },
+    {
+        "href":"https://bbs.mihoyo.com/ys/obc/channel/map/80/81?bbs_presentation_style=no_header",
+        "cont":"原神·过场动画"
+    },
+    {
+        "href":"https://yoyys1.com/",
+        "cont":"悠云影视"
+    },
+    {
+        "href":"https://36kdh.com/",
+        "cont":"36K导航"
+    },
+    {
+        "href":"https://1080zyk3.com/",
+        "cont":"优质资源库·内置广告多"
+    },
+    {
+        "href":"https://1029yy.com/",
+        "cont":"1029影院"
+    },
+    {
+        "href":"https://www.pptv06.com/",
+        "cont":"PPTV66"
+    },
+    {
+        "href":"https://www.xia2024.com/",
+        "cont":"影视资源下载"
+    },
+    {
+        "href":"https://www.discuz.vip/",
+        "cont":"Discuz社区"
+    },
+    {
+        "href":"https://www.fanshudm.com/",
+        "cont":"番薯动漫_您追番的最佳归属"
+    },
+    {
+        "href":"https://www.ziyedm.com/",
+        "cont":"子夜动漫·A"
+    },
+    {
+		"href" : "https://www.dy2018.com/",
+		"cont" : "电影天堂"
+	},
+	{
+		"href" : "https://www.lookae.com/",
+		"cont" : "LookAE.com-大众脸影视后期特效"
+	},
+	{
+		"href" : "https://4kysxz.top/",
+		"cont" : "4K影视小站"
+	},
+	{
+		"href" : "https://www.oftens.top/",
+		"cont" : "往往影视"
+	},
+	{
+		"href" : "https://klyingshi.com/",
+		"cont" : "可乐影视"
+	},
+	{
+		"href" : "https://www.cangkus.com/",
+		"cont" : "经典仓库-音乐影视"
+	},
+	{
+		"href" : "https://lgyy.vip/",
+		"cont" : "蓝光影院"
+	},
+	{
+		"href" : "https://www.mfdy.com",
+		"cont" : "MFDY免费电影导航"
+	},
+	{
+		"href" : "https://ymck.me/",
+		"cont" : "影猫の仓库"
+	},
+	// {
+	// 	"href" : "https://www.ikyy.tv/",
+	// 	"cont" : "爱看影院"
+	// },
+	{
+		"href" : "https://www.zhihu4k.com/",
+		"cont" : "纸虎4K影视"
+	},
+	{
+		"href" : "https://www.taozi007.com/",
+		"cont" : "桃子影视"
+	},
+	{
+		"href" : "https://gaze.run/",
+		"cont" : "注视影视"
+	},
+	{
+		"href" : "https://www.3ayy.com/",
+		"cont" : "爱看影院"
+	},
+	{
+		"href" : "https://www.yingshi.tv/",
+		"cont" : "影视TV"
+	},
+	{
+		"href" : "https://www.mayi4k.com/",
+		"cont" : "蚂蚁4K"
+	},
+	{
+		"href" : "https://www.4khdr.cn/",
+		"cont" : "4K·HDR世界"
+	},
+	{
+		"href" : "https://newfii.com/",
+		"cont" : "newfii"
+	},
+	{
+		"href" : "https://www.grab4k.com/",
+		"cont" : "grab4k"
+	},
+    {
+		"href" : "https://www.cupfox.app",
+		"cont" : "茶杯狐"
+	},
+	{
+		"href" : "https://www.dadagui.me/",
+		"cont" : "达达龟"
+	},
+    {
+        "href":"http://www.snrtv.com/",
+        "title":"陕西网络广播电视台"
+    },
+	{
+		"href" : "https://www.voflix.me/",
+		"cont" : "voflix HD"
+	},
+	{
+		"href" : "https://www.pkmp4.xyz/",
+		"cont" : "片库|七味"
+	},
+	{
+		"href" : "https://www.novipnoad.net/",
+		"cont" : "NO视频"
+	},
+	{
+		"href" : "https://www.zjtu.tv/",
+		"cont" : "追剧兔"
+	},
+	{
+		"href" : "https://abc2022.cn/",
+		"cont" : "怀旧动画"
+	},
+	{
+		"href" : "https://soupian.plus/",
+		"cont" : "搜片.com"
+	},
+	{
+		"href" : "https://www.kkw361.com/",
+		"cont" : "看看屋"
+	},
+	{
+		"href" : "https://www.xinjuc.com/",
+		"cont" : "新剧场"
+	},
+	{
+		"href" : "https://www.4kvm.net",
+		"cont" : "4kvm"
+	},
+	{
+		"href" : "https://www.4kvm.site",
+		"cont" : "A-moive|4kvm"
+	},
+	{
+		"href" : "https://www.dbmp4.com/",
+		"cont" : "A-moive|dbmp4"
+	},
+	{
+		"href" : "https://www.05jl.com/",
+		"cont" : "记录片下载"
+	},
+	{
+		"href" : "https://www.24kdh.vip/",
+		"cont" : "24K导航"
+	},
+	{
+		"href" : "https://www.bugutv.org/",
+		"cont" : "布谷TV"
+	},
+    {
+        "href":"http://svip.zhuiying.cyou/",
+        "cont":"A-moive|追影视频"
+    },
+    {
+        "href":"https://www.6umkq9b.com/",
+        "cont":"A-movie|金牌影院-a"
+    },
+    {
+        "href":"https://www.shifante.com/",
+        "cont":"A-movie|金牌影院-b"
+    },
+    {
+        "href":"https://www.ruiteqiche.com/",
+        "cont":"A-movie|金牌影院-c"
+    },
+    {
+        "href":"https://kimivod.com/",
+        "cont":"A-moive|kimivod·电影电视剧"
+    },
+    {
+        "href":"https://bttwo.vip/",
+        "cont":"A-moive|BT影视"
+    },
+    {
+        "href":"https://www.meijumi.net/",
+        "cont":"A-moive|美剧迷"
+    },
+    {
+        "href":"https://www.modianyyds.com/",
+        "cont":"A-moive|墨点影视"
+    },
+    {
+        "href":"https://haikuoshijie.cn/archives/fang-yuan-ying-shi-chang-jian-wen-ti",
+        "cont":"小棉袄软件汇总"
+    },
+    {
+        "href":"https://dlpanda.com/zh-CN",
+        "cont":"A-movie|在线下载抖音"
+    },
+    {
+        "href":"https://douyin.wtf/docs#/",
+        "cont":"Douyin TikTok Download API"
+    },
+    {
+        "href":"https://douyin.wtf/",
+        "cont":"Douyin TikTok Download API"
+    },
+    {
+        "href":"https://www.mcoun.com/",
+        "cont":"A-moive|清风影院·proxy"
+    },
+    {
+        "href":"https://www.wobotv.com/",
+        "cont":"A-moive|我播"
+    },
+    {
+        "href":"https://bttwo.vip/",
+        "cont":"A|BT影视"
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+    {
+        "href":"",
+        "cont":""
+    },
+];
